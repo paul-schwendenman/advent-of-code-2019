ee1fda43bd15ac07fd3fc608f91661e28092d89b
Paul Schwendenman <schwendenman.paul@gmail.com>
2019-12-01 16:47:15 -0500

    Add other examples

--- day01/test_day01.py
+++ day01/test_day01.py
@@ -5,3 +5,9 @@ def test_example_1():
 
 def test_example_2():
     assert calculate_fuel(14) == 2
+
+def test_example_3():
+    assert calculate_fuel(1969) == 654
+
+def test_example_4():
+    assert calculate_fuel(100756) == 33583
