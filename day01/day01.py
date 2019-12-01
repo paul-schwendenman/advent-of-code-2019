81e049423bad209d44acc71bc30c43ad18e02fb1
Paul Schwendenman <schwendenman.paul@gmail.com>
2019-12-01 16:43:39 -0500

    Add skeleton for day 1

---
+++ day01/day01.py
@@ -0,0 +1,15 @@
+def calculate_fuel(module_mass):
+    return 2
+
+
+def main():
+    pass
+    # load input file
+
+    # execute calculation
+
+    # sum fuel values
+
+
+if __name__ == '__main__':
+    main()
---
+++ day01/test_day01.py
@@ -0,0 +1,4 @@
+from day01 import calculate_fuel
+
+def test_example_1():
+    assert calculate_fuel(12) == 2
