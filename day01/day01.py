1554a52a440e87ea5cd851b8f7a57d7028cf9864
Paul Schwendenman <schwendenman.paul@gmail.com>
2019-12-01 16:44:04 -0500

    Add implementation

--- day01/day01.py
+++ day01/day01.py
@@ -1,5 +1,7 @@
+import math
+
 def calculate_fuel(module_mass):
-    return 2
+    return math.floor(module_mass / 3) - 2
 
 
 def main():
--- day01/test_day01.py
+++ day01/test_day01.py
@@ -2,3 +2,6 @@ from day01 import calculate_fuel
 
 def test_example_1():
     assert calculate_fuel(12) == 2
+
+def test_example_2():
+    assert calculate_fuel(14) == 2
