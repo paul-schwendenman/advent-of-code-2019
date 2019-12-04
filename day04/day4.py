024d2125dbe3e43d903c2f8214a85e0e4a17825d
Paul Schwendenman <schwendenman.paul@gmail.com>
2019-12-04 01:22:04 -0500

    Refactor to remove useless helper function

--- day04/day4.py
+++ day04/day4.py
@@ -3,18 +3,17 @@ def has_double_digit(number):
     return any([number[counter+1] == digit for counter, digit in enumerate(number[:-1])])
 
 
-def get_sets(array):
-    return [(item, array[counter+1], array[counter+2], array[counter+3]) for counter, item in enumerate(array[:-3])]
-
-
 def check_four(a, b, c, d):
     return b == c and a != b and c != d
 
 
 def has_double_digit2(number):
     number = str(number)
-    sets = get_sets(number)
+    sets = []
     sets.append((None, number[0], number[1], number[2]))
+    sets.append((number[0], number[1], number[2], number[3]))
+    sets.append((number[1], number[2], number[3], number[4]))
+    sets.append((number[2], number[3], number[4], number[5]))
     sets.append((number[3], number[4], number[5], None))
 
     return any(check_four(a, b, c, d) for a, b, c, d in sets)
