ed8811b214695356f5a88e49502b1af308d27f5a
Paul Schwendenman <schwendenman.paul@gmail.com>
2019-12-04 01:15:49 -0500

    Add solution for puzzle 2

--- day04/day4.py
+++ day04/day4.py
@@ -3,13 +3,30 @@ def has_double_digit(number):
     return any([number[counter+1] == digit for counter, digit in enumerate(number[:-1])])
 
 
+def get_sets(array):
+    return [(item, array[counter+1], array[counter+2], array[counter+3]) for counter, item in enumerate(array[:-3])]
+
+
+def check_four(a, b, c, d):
+    return b == c and a != b and c != d
+
+
+def has_double_digit2(number):
+    number = str(number)
+    sets = get_sets(number)
+    sets.append((None, number[0], number[1], number[2]))
+    sets.append((number[3], number[4], number[5], None))
+
+    return any(check_four(a, b, c, d) for a, b, c, d in sets)
+
+
 def never_decreases(number):
     number = str(number)
     return all([number[counter+1] >= digit for counter, digit in enumerate(number[:-1])])
 
 
 def main():
-    count = sum(1 for number in range(248345, 746315) if has_double_digit(number) and never_decreases(number))
+    count = sum(1 for number in range(248345, 746315) if has_double_digit2(number) and never_decreases(number))
     print(count)
 
 
--- day04/test_day4.py
+++ day04/test_day4.py
@@ -1,4 +1,4 @@
-from day4 import has_double_digit, never_decreases
+from day4 import has_double_digit, has_double_digit2, never_decreases, get_sets
 
 
 def test_has_double_digit():
@@ -7,6 +7,22 @@ def test_has_double_digit():
 
 def test_does_not_have_double_digit():
     assert not has_double_digit(123456)
+
+
+# def test_get_sets():
+#     assert get_sets("123456") == [("1", "2", "3"), ("2", "3", "4"), ("3", "4", "5"), ("4", "5", "6"),]
+
+
+def test_has_double_digit2():
+    assert has_double_digit2(112233)
+
+
+def test_has_double_digit2_2():
+    assert has_double_digit2(111144)
+
+
+def test_does_not_have_double_digit2():
+    assert not has_double_digit2(123444)
 
 
 def test_never_decreases():
