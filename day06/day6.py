0c6cfcedb36fb1bd20bcc1ccef91180ccabe0373
Paul Schwendenman <schwendenman.paul@gmail.com>
2019-12-06 00:44:15 -0500

    Add first pass

--- day06/day6.py
+++ day06/day6.py
@@ -1,8 +1,39 @@
+from collections import defaultdict
+
+
+def create_map(orbits):
+    orbit_map = defaultdict(list)
+
+    for orbit in orbits:
+        parent, child = orbit.strip().split(')')
+        orbit_map[parent].append(child)
+
+    return orbit_map
+
+
+def count_orbits(orbit_map, key="COM", depth=0):
+    # print(f'key: {key} map: {orbit_map}')
+    if key in orbit_map:
+        total = sum([count_orbits(orbit_map, child, depth+1) for child in orbit_map[key]])
+        print(f'key {key} total {total}')
+        # print(f'key: {key} direct: {direct} indirect: {indirect}')
+        # return direct + indirect
+        return total + depth
+    else:
+        print(f'stop - key {key}, {depth}')
+        return depth
+
+
+def calc_orbits(input):
+    return count_orbits(create_map(input))
 
 
 def main():
-    pass
+    with open('day06/input') as input:
+        orbits = calc_orbits(input.readlines())
+
+    return orbits
 
 
 if __name__ == "__main__":
-    main()
+    print(main())
--- day06/test_day6.py
+++ day06/test_day6.py
@@ -1,5 +1,10 @@
-from day6 import main
+from day6 import main, calc_orbits
 
 
-def test_main():
-    assert main() is None
+# def test_mais
+
+def test_calc_orbits():
+    assert calc_orbits(["COM)B", "B)C"]) == 3
+
+# def test_calc_orbits2():
+#     assert calc_orbits(["COM)B", "B)C", "C)D"]) == 6
