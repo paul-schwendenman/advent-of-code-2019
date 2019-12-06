6a789b7ad954f509fb26fab0b7045626ef3cb779
Paul Schwendenman <schwendenman.paul@gmail.com>
2019-12-06 01:15:31 -0500

    Add part2 solution

--- day06/day6.py
+++ day06/day6.py
@@ -24,16 +24,50 @@ def count_orbits(orbit_map, key="COM", depth=0):
         return depth
 
 
+def find_path(orbit_map, start="COM", end="YOU"):
+    print(f'{start} -> {end}')
+    if end in orbit_map[start]:
+        print('got \'em')
+        return [start]
+    else:
+        for child in orbit_map[start]:
+            path = find_path(orbit_map, child, end)
+            if path:
+                path.append(start)
+                return path
+        else:
+            return None
+
+
 def calc_orbits(input):
     return count_orbits(create_map(input))
 
 
+def calc_hops(lines):
+    orbit_map = create_map(lines)
+
+    you_path = set(find_path(orbit_map))
+    san_path = set(find_path(orbit_map, end="SAN"))
+
+    return len(you_path.symmetric_difference(san_path))
+
+
 def main():
     with open('day06/input') as input:
-        orbits = calc_orbits(input.readlines())
+        lines = input.readlines()
+
+    orbits = calc_orbits(lines)
 
     return orbits
 
 
+def main2():
+    with open('day06/input') as input:
+        lines = input.readlines()
+
+    return calc_hops(lines)
+
+
 if __name__ == "__main__":
-    print(main())
+    # print(main())
+    print(main2())
--- day06/test_day6.py
+++ day06/test_day6.py
@@ -1,4 +1,4 @@
-from day6 import main, calc_orbits
+from day6 import main, calc_orbits, calc_hops
 
 
 # def test_mais
@@ -6,8 +6,12 @@ from day6 import main, calc_orbits
 def test_calc_orbits():
     assert calc_orbits(["COM)B", "B)C"]) == 3
 
+
 def test_calc_orbits2():
     assert calc_orbits(['COM)B\n', 'B)C\n', 'C)D\n', 'D)E\n', 'E)F\n', 'B)G\n', 'G)H\n', 'D)I\n', 'E)J\n', 'J)K\n', 'K)L\n']) == 42
 
+
+def test_calc_hops():
+    assert calc_hops(['COM)B\n', 'B)C\n', 'C)D\n', 'D)E\n', 'E)F\n', 'B)G\n', 'G)H\n', 'D)I\n', 'E)J\n', 'J)K\n', 'K)L\n', 'K)YOU\n', 'I)SAN\n']) == 4
 # def test_calc_orbits2():
 #     assert calc_orbits(["COM)B", "B)C", "C)D"]) == 6
