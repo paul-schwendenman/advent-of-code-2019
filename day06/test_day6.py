45e31a31f2acb1b46bf23e34687d82815eef0a73
Paul Schwendenman <schwendenman.paul@gmail.com>
2019-12-06 00:45:39 -0500

    Add test from example

--- day06/test_day6.py
+++ day06/test_day6.py
@@ -6,5 +6,8 @@ from day6 import main, calc_orbits
 def test_calc_orbits():
     assert calc_orbits(["COM)B", "B)C"]) == 3
 
+def test_calc_orbits2():
+    assert calc_orbits(['COM)B\n', 'B)C\n', 'C)D\n', 'D)E\n', 'E)F\n', 'B)G\n', 'G)H\n', 'D)I\n', 'E)J\n', 'J)K\n', 'K)L\n']) == 42
+
 # def test_calc_orbits2():
 #     assert calc_orbits(["COM)B", "B)C", "C)D"]) == 6
