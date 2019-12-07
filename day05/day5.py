5eb5142bbc175a1b22523bb4eebc194df10f3a9d
Paul Schwendenman <schwendenman.paul@gmail.com>
2019-12-06 21:59:14 -0500

    Refactor to extract parameter assignment

--- day05/day5.py
+++ day05/day5.py
@@ -11,26 +11,27 @@ def split_instruction(instruction):
 
 
 def lookup_value(memory, mode, position):
-    if mode == '0':
-        return memory[memory[position]]
-    elif mode == '1':
-        return memory[position]
+    try:
+        if mode == '0':
+            return memory[memory[position]]
+        elif mode == '1':
+            return memory[position]
+    except IndexError:
+        pass
 
 
 def run_program(program, cursor=0, input_value=5):
     opcode, parameter_modes = split_instruction(program[cursor])
+    param_1 = lookup_value(program, parameter_modes[0], cursor + 1)
+    param_2 = lookup_value(program, parameter_modes[1], cursor + 2)
 
     if opcode == 99:
         return program
     elif opcode == 1:
-        param_1 = lookup_value(program, parameter_modes[0], cursor + 1)
-        param_2 = lookup_value(program, parameter_modes[1], cursor + 2)
         program[program[cursor + 3]] = param_1 + param_2
 
         return run_program(program, cursor + 4, input_value)
     elif opcode == 2:
-        param_1 = lookup_value(program, parameter_modes[0], cursor + 1)
-        param_2 = lookup_value(program, parameter_modes[1], cursor + 2)
         program[program[cursor + 3]] = param_1 * param_2
 
         return run_program(program, cursor + 4, input_value)
@@ -43,33 +44,24 @@ def run_program(program, cursor=0, input_value=5):
 
         return run_program(program, cursor + 2, input_value)
     elif opcode == 4:
-        param_1 = lookup_value(program, parameter_modes[0], cursor + 1)
         print(param_1)
 
         return run_program(program, cursor + 2, input_value)
     elif opcode == 5:
-        param_1 = lookup_value(program, parameter_modes[0], cursor + 1)
-        param_2 = lookup_value(program, parameter_modes[1], cursor + 2)
         if param_1:
             return run_program(program, param_2, input_value)
 
         return run_program(program, cursor + 3, input_value)
     elif opcode == 6:
-        param_1 = lookup_value(program, parameter_modes[0], cursor + 1)
-        param_2 = lookup_value(program, parameter_modes[1], cursor + 2)
         if not param_1:
             return run_program(program, param_2, input_value)
 
         return run_program(program, cursor + 3, input_value)
     elif opcode == 7:
-        param_1 = lookup_value(program, parameter_modes[0], cursor + 1)
-        param_2 = lookup_value(program, parameter_modes[1], cursor + 2)
         program[program[cursor + 3]] = 1 if param_1 < param_2 else 0
 
         return run_program(program, cursor + 4, input_value)
     elif opcode == 8:
-        param_1 = lookup_value(program, parameter_modes[0], cursor + 1)
-        param_2 = lookup_value(program, parameter_modes[1], cursor + 2)
         program[program[cursor + 3]] = 1 if param_1 == param_2 else 0
 
         return run_program(program, cursor + 4, input_value)
