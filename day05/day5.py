f907b362ac8e3f4f5e92939e560d14065879250a
Paul Schwendenman <schwendenman.paul@gmail.com>
2019-12-06 22:12:46 -0500

    Refactor to make parameter access more useful

--- day05/day5.py
+++ day05/day5.py
@@ -20,9 +20,9 @@ def split_instruction(instruction):
 def lookup_value(memory, mode, position):
     try:
         if mode == '0':
-            return memory[memory[position]]
-        elif mode == '1':
             return memory[position]
+        elif mode == '1':
+            return position
     except IndexError:
         pass
 
@@ -31,41 +31,42 @@ def run_program(program, cursor=0, input_value=5):
     opcode, parameter_modes = split_instruction(program[cursor])
     param_1 = lookup_value(program, parameter_modes[0], cursor + 1)
     param_2 = lookup_value(program, parameter_modes[1], cursor + 2)
+    param_3 = lookup_value(program, parameter_modes[2], cursor + 3)
 
     if opcode == 99:
         return program
     elif opcode == 1:
-        program[program[cursor + 3]] = param_1 + param_2
+        program[param_3] = program[param_1] + program[param_2]
 
         return run_program(program, cursor + 4, input_value)
     elif opcode == 2:
-        program[program[cursor + 3]] = param_1 * param_2
+        program[param_3] = program[param_1] * program[param_2]
 
         return run_program(program, cursor + 4, input_value)
     elif opcode == 3:
-        program[program[cursor + 1]] = input_value
+        program[param_1] = input_value
 
         return run_program(program, cursor + 2, input_value)
     elif opcode == 4:
-        print(param_1)
+        print(program[param_1])
 
         return run_program(program, cursor + 2, input_value)
     elif opcode == 5:
-        if param_1:
-            return run_program(program, param_2, input_value)
+        if program[param_1]:
+            return run_program(program, program[param_2], input_value)
 
         return run_program(program, cursor + 3, input_value)
     elif opcode == 6:
-        if not param_1:
-            return run_program(program, param_2, input_value)
+        if not program[param_1]:
+            return run_program(program, program[param_2], input_value)
 
         return run_program(program, cursor + 3, input_value)
     elif opcode == 7:
-        program[program[cursor + 3]] = 1 if param_1 < param_2 else 0
+        program[param_3] = 1 if program[param_1] < program[param_2] else 0
 
         return run_program(program, cursor + 4, input_value)
     elif opcode == 8:
-        program[program[cursor + 3]] = 1 if param_1 == param_2 else 0
+        program[param_3] = 1 if program[param_1] == program[param_2] else 0
 
         return run_program(program, cursor + 4, input_value)
     else:
