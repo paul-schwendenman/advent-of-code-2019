fed4f0c27d69e006b220ba0282348cbe1a1c4137
Paul Schwendenman <schwendenman.paul@gmail.com>
2019-12-08 03:11:57 -0500

    Add part2

--- day08/day8.py
+++ day08/day8.py
@@ -28,7 +28,34 @@ def main():
     print(answer['1'] * answer['2'])
 
 
+def main2():
+    with open('input') as file:
+        data = list(file.read())[:-1]
+
+    dimensions = 25 * 6
+
+    layers = [data[i:i+dimensions] for i in range(0, len(data), dimensions)]
+
+    image = ['2'] * 150
+
+    for layer in layers:
+        for index, pixel in enumerate(layer):
+            if image[index] == '2':
+                image[index] = pixel
+            else:
+                pass
+
+    image_rows = [image[i:i+25] for i in range(0, len(image), 25)]
+
+    for row in image_rows:
+        for pixel in row:
+            if pixel == '0':
+                print(' ', end='')
+            else:
+                print('#', end='')
+        print('')
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    main2()
