e2dc98b3d147e36bf1893e4b295accc0ff7a4ea1
Paul Schwendenman <schwendenman.paul@gmail.com>
2019-12-11 23:58:53 -0500

    Add part2 test

--- day11/test_day11.py
+++ day11/test_day11.py
@@ -1,5 +1,11 @@
 from day11 import main
 
 
-def test_main():
+def test_main_part1():
     assert main() == 2319
+
+
+def test_main_part2(capsys):
+    main(1)
+    captured = capsys.readouterr()
+    assert captured.out == ' #  # #### ###  ###  ###  ####  ##    ##   \n #  # #    #  # #  # #  # #    #  #    #   \n #  # ###  #  # #  # #  # ###  #       #   \n #  # #    ###  ###  ###  #    # ##    #   \n #  # #    # #  #    # #  #    #  # #  #   \n  ##  #### #  # #    #  # #     ###  ##    \n'  # noqa: E501
