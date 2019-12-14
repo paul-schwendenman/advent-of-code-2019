5050dd0bd0a5a94194a24db88144477e21f9131b
Paul Schwendenman <schwendenman.paul@gmail.com>
2019-12-14 11:34:53 -0500

    Add solution for example 2

--- day14/day14.py
+++ day14/day14.py
@@ -1,27 +1,64 @@
 from collections import namedtuple, defaultdict
 import math
+from time import sleep
 
 Formula = namedtuple('Formula', 'product reactants')
 Product = namedtuple('Product', 'quantity name')
 Reactant = namedtuple('Reactant', 'quantity name')
 
 
-def find_reactants(formulas, need, produced, product='FUEL'):
-    print(f'product: {product} need: {need} produced: {produced}')
-    if product == 'ORE':
-        return need
-    else:
-        formula = formulas[product]
-        if produced[product] < need:
-            multiplier = math.ceil(need / formula.product.quantity)
-        else:
-            multiplier = 0
+def find_reactants(formulas, needed_quantity=1, product='FUEL'):
 
-        produced[product] += (multiplier * formula.product.quantity) - need
+    needs = [product]
+    # quantity = {product: needed_quantity}
+    quantity = defaultdict(int)
+    quantity[product] = needed_quantity
+    # extra = defaultdict(int)
+    extra = {}
 
-        needs = {reactant.name: multiplier * reactant.quantity for reactant in formula.reactants}
+    while needs != ['ORE']:
+        # print(f'{needs} {quantity} {extra}')
+        need = needs.pop(0)
 
-        return sum(find_reactants(formulas, needs[reactant.name], produced, reactant.name) for reactant in formula.reactants)
+        if need == 'ORE':
+            # needs.append(need)
+            continue
+
+        formula = formulas[need]
+        multiplier = math.ceil((quantity[need] - extra.get(need, 0)) / formula.product.quantity)
+        # print(f'multi: {multiplier} = ceiling(({quantity[need]} - {extra.get(need, 0)}) / {formula.product.quantity})')
+
+        excess_produced = (multiplier * formula.product.quantity) - quantity[need]
+        if excess_produced:
+            extra[need] = excess_produced
+        # print(f'extra: {extra[need] if need in extra else None} = (({multiplier} * {formula.product.quantity}) - {quantity[need]})')
+
+        for reactant in formula.reactants:
+            quantity[reactant.name] += (multiplier * reactant.quantity) - extra.get(reactant.name, 0)
+            if reactant.name not in needs:
+                needs.append(reactant.name)
+
+        del quantity[need]
+
+    return quantity[needs[0]]
+
+    # print(f'product: {product} need: {need} produced: {produced}')
+    # if product == 'ORE':
+    #     return need
+    # else:
+    #     formula = formulas[product]
+    #     if extra_produced[product] < need:
+    #         multiplier = math.ceil(need / formula.product.quantity)
+    #     else:
+    #         return 0
+
+    #     extra_produced[product] += (multiplier * formula.product.quantity) - need
+
+    #     needs = {reactant.name: multiplier * reactant.quantity for reactant in formula.reactants}
+
+    #     print(f'consume {", ".join(f"{v} {k}" for k,v in needs.items())} to produce {need} {product} with {extra_produced[product]} extra')
+
+    #     return sum(find_reactants(formulas, needs[reactant.name], extra_produced, reactant.name) for reactant in formula.reactants)
 
 
 def main(filename="puzzle1.in"):
@@ -36,10 +73,11 @@ def main(filename="puzzle1.in"):
     for key in formulas.keys():
         print(f'{key}: {formulas[key]}')
 
-    produced = defaultdict(int)
+    # produced = defaultdict(int)
 
-    return find_reactants(formulas, 1, produced)
+    return find_reactants(formulas)
 
 
 if __name__ == "__main__":
-    print(main())
+    # print(main())
+    print(main('example1.in'))
--- day14/test_day14.py
+++ day14/test_day14.py
@@ -3,3 +3,7 @@ from day14 import main
 
 def test_main_example_1():
     assert main('example1.in') == 31
+
+
+def test_main_example_2():
+    assert main('example2.in') == 165
