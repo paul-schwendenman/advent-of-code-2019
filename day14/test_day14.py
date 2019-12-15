d9b27024b72c588da483b439b10f994fad9d667f
Paul Schwendenman <schwendenman.paul@gmail.com>
2019-12-15 13:55:04 -0500

    Fix day14 tests

--- day14/test_day14.py
+++ day14/test_day14.py
@@ -2,32 +2,32 @@ from day14 import main, main2
 
 
 def test_main_example_1():
-    assert main('example1.in') == 31
+    assert main('day14/example1.in') == 31
 
 
 def test_main_example_2():
-    assert main('example2.in') == 165
+    assert main('day14/example2.in') == 165
 
 
 def test_main_example_3():
-    assert main('example3.in') == 13312
+    assert main('day14/example3.in') == 13312
 
 
 def test_main_example_4():
-    assert main('example4.in') == 180697
+    assert main('day14/example4.in') == 180697
 
 
 def test_main_puzzle_input():
-    assert main('puzzle1.in') == 374457
+    assert main('day14/puzzle1.in') == 374457
 
 
 def test_main2_example_3():
-    assert main2('example3.in') == 82892753
+    assert main2('day14/example3.in') == 82892753
 
 
 def test_main2_example_4():
-    assert main2('example4.in') == 5586022
+    assert main2('day14/example4.in') == 5586022
 
 
 def test_main2_puzzle_input():
-    assert main2('puzzle1.in') == 3568888
+    assert main2('day14/puzzle1.in') == 3568888
