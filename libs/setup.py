e02b02b7f43449b5780ca2e64fbc566f53980a36
Paul Schwendenman <schwendenman.paul@gmail.com>
2019-12-15 13:00:41 -0500

    Add intcode package

---
+++ libs/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# Copyright 2019 Paul Schwendenman. All Rights Reserved.
+
+from setuptools import setup
+import intcode
+
+
+setup(name='intcode',
+      version=intcode.__version__,
+      description=intcode.__doc__.strip(),
+      long_description='',
+      author=intcode.__author__,
+      author_email=intcode.__email__,
+      license=intcode.__license__,
+      url='https://github.com/paul-schwendenman/advent-of-code-2019',
+      packages=['intcode'],
+      classifiers=[],
+      )
