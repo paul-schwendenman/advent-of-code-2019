f342d7a679c8b9fec7d99b7c8e516a64bd033ec6
Paul Schwendenman <pschwendenman@pillartechnology.com>
2019-12-19 16:40:38 -0500

    Add part 1 solution

--- day18/day18.py
+++ day18/day18.py
@@ -1,8 +1,13 @@
 from enum import Enum
 from collections import namedtuple, defaultdict
+from itertools import count
+import math
+from typing import List, Iterator, Dict
 
 
 Point = namedtuple('Point', 'x y')
+Path = namedtuple('Path', 'count group')
+Location = namedtuple('Location', 'coords group')
 
 
 class MazeItem(Enum):
@@ -11,36 +16,102 @@ class MazeItem(Enum):
     STONE_WALL = '#'
 
 
+def get_neighboors(location: Point) -> Iterator[Point]:
+    yield Point(location.x - 1, location.y)
+    yield Point(location.x + 1, location.y)
+    yield Point(location.x, location.y - 1)
+    yield Point(location.x, location.y + 1)
+
+
 def process_maze(template):
-    keys = set()
-    doors = set()
-    entrance = None
-    path = set()
+    maze = {}
+    goals = {}
 
     for y, row in enumerate(template):
         for x, item in enumerate(row):
-            try:
-                location = MazeItem(item)
-            except ValueError:
-                if item.isupper():
-                    doors.add(Point(x, y))
-                else:
-                    keys.add(Point(x, y))
+            point = Point(x, y)
+            maze[point] = item
+            if item not in ('.', '#'):
+                goals[item] = point
 
-            if location == MazeItem.ENTRANCE:
-                entrance = Point(x, y)
-            elif location == MazeItem.OPEN_PASSAGE:
-                path.add(Point(x, y))
+    return maze, goals
 
-    return keys, doors, entrance, path
+
+def find_paths(maze, start):
+    paths: Dict[str, Path] = {}
+    route: Dict[Point, Path] = defaultdict(lambda: Path(math.inf, set()))
+    route[start] = Path(0, set())
+    next_location: List[Location] = [Location(start, set())]
+
+    for step in count(1):
+        if not next_location:
+            break
+
+        current_locations, next_location = next_location, []
+
+        for current_location, ds in current_locations:
+            for neighboor in get_neighboors(current_location):
+                item = maze[neighboor]
+                if item == '#' or route[neighboor][0] <= step:
+                    continue
+                if item.islower():  # Check if key
+                    paths[item] = (step, ds)
+                new_ds = ds
+                if item.isupper():  # Check if door
+                    new_ds = new_ds | {item.lower()}
+                route[neighboor] = (step, new_ds)
+                next_location.append(Location(neighboor, new_ds))
+
+    return paths
 
 
 def main(filename="input"):
     with open(filename) as input_file:
         grid_template = input_file.read().splitlines()
 
-    process_maze(grid_template)
+    maze, goals = process_maze(grid_template)
+
+    keys = {key for key in goals.keys() if key.islower()}
+    links = {
+        '@': find_paths(maze, goals['@'])
+    }
+
+    for key in keys:
+        links[key] = find_paths(maze, goals[key])
+
+    cache = {}
+
+    def walk_path(name, need_keys):
+        if not need_keys:
+            return 0
+
+        cache_key = name + ''.join(need_keys)
+        if cache_key in cache:
+            return cache[cache_key]
+
+        shortest = math.inf
+        for key in need_keys:
+            path_length, doors = links[name][key]
+
+            if path_length >= shortest:
+                continue
+
+            if not doors.isdisjoint(need_keys):
+                continue
+
+            tail = walk_path(key, need_keys - {key})
+
+            if shortest > (path_length + tail):
+                shortest = path_length + tail
+
+        cache[cache_key] = shortest
+        return shortest
+
+    result = walk_path('@', keys)
+    print(f'cached: {len(cache)}')
+
+    return result
 
 
 if __name__ == "__main__":
-    main()
+    print(main())
