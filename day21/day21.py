62350ca4e99233d27870fea187f074f25fa846b8
Paul Schwendenman <schwendenman.paul@gmail.com>
2019-12-21 00:54:40 -0500

    Add part b solution

--- day21/day21.py
+++ day21/day21.py
@@ -18,7 +18,16 @@ def main():
     computer._inputs.extend(string_to_ascii('NOT C T\n'))
     computer._inputs.extend(string_to_ascii('OR T J\n'))
     computer._inputs.extend(string_to_ascii('AND D J\n'))
-    computer._inputs.extend(string_to_ascii('WALK\n'))
+    # computer._inputs.extend(string_to_ascii('WALK\n'))
+
+    # J = J and (D and (E | H))
+
+    computer._inputs.extend(string_to_ascii('NOT E T\n'))
+    computer._inputs.extend(string_to_ascii('NOT T T\n'))
+    computer._inputs.extend(string_to_ascii('OR H T\n'))
+    computer._inputs.extend(string_to_ascii('AND D T\n'))
+    computer._inputs.extend(string_to_ascii('AND T J\n'))
+    computer._inputs.extend(string_to_ascii('RUN\n'))
 
     computer.run()
 
