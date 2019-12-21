dea48ae7f9ebe8e4fd21e2d446f2499e380feeaf
Paul Schwendenman <schwendenman.paul@gmail.com>
2019-12-21 00:44:52 -0500

    Add part 1 solution

---
+++ day21/day21.py
@@ -0,0 +1,34 @@
+from intcode import IntCode, open_program
+
+
+def string_to_ascii(command):
+    return map(ord, command)
+
+
+def main():
+    program = open_program('input')
+
+    computer = IntCode(program, default_memory=8000)
+
+    # J = (~A or ~B or ~C) and D
+
+    computer._inputs.extend(string_to_ascii('NOT A T\n'))
+    computer._inputs.extend(string_to_ascii('NOT B J\n'))
+    computer._inputs.extend(string_to_ascii('OR T J\n'))
+    computer._inputs.extend(string_to_ascii('NOT C T\n'))
+    computer._inputs.extend(string_to_ascii('OR T J\n'))
+    computer._inputs.extend(string_to_ascii('AND D J\n'))
+    computer._inputs.extend(string_to_ascii('WALK\n'))
+
+    computer.run()
+
+    for char in computer._outputs:
+        try:
+            print(chr(char), end='')
+        except ValueError:
+            return char
+
+
+
+if __name__ == "__main__":
+    print(main())
