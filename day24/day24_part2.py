e9d76c0cd590ef85f2b0c3cb1a235f733b908c7a
Paul Schwendenman <schwendenman.paul@gmail.com>
2019-12-24 02:45:06 -0500

    WIP

--- day24/day24_part2.py
+++ day24/day24_part2.py
@@ -14,12 +14,35 @@ def load_intial_state(filename='input'):
     return state
 
 
+def get_neighboors_above(states, x, y, depth):
+    if x == 0:
+        yield states[depth][2][1]
+    elif x == 4:
+        yield states[depth][2][3]
+    if y == 0:
+        yield states[depth][1][2]
+    elif y == 4:
+        yield states[depth][3][2]
+
+
+def get_neighboors_below(states, x, y, depth):
+    if x == 2 and y == 1:
+        yield from [states[depth][0][x] for x in range(5)]
+    elif x == 2 and y == 3:
+        yield from [states[depth][4][x] for x in range(5)]
+    elif x == 1 and y == 2:
+        yield from [states[depth][y][0] for y in range(5)]
+    elif x == 3 and y == 2:
+        yield from [states[depth][y][4] for y in range(5)]
+
+
 def get_neighboors(states, x, y, depth):
     yield states[depth][y-1][x]
     yield states[depth][y][x-1]
     yield states[depth][y+1][x]
     yield states[depth][y][x+1]
-
+    yield from get_neighboors_above(states, x, y, depth - 1)
+    yield from get_neighboors_below(states, x, y, depth + 1)
 
 
 def count_neighboors(states, x, y, depth):
@@ -34,9 +57,12 @@ def count_neighboors(states, x, y, depth):
 
 def step_simulation(states):
     new_states = defaultdict(lambda: defaultdict(lambda: defaultdict(bool)))
-    for depth, state in states.items():
+    for depth in range(-10, 11):
+        state = states[depth]
         for y in range(5):
             for x in range(5):
+                if y == 2 and x == 2:
+                    continue
                 space = state[y][x]
                 count = count_neighboors(states, x, y, depth)
                 if space and count == 1:
@@ -85,6 +111,7 @@ def main():
         states = step_simulation(states)
 
         print_grid(states)
+        print('__________________________')
 
         score = count_bugs(states)
 
